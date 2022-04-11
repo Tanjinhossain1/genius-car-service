53d5fbd0d5e8ebd623a243fc28d688315f9bc61d
tanjinhossain1 <tanjinhossain2003@gmail.com>
2022-04-11 11:59:50 +0600

    added some componet and show some car services

---
+++ src/firebase.init.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBWVoXJ2DLbFdOanrEVctbmk3ISbmdLJmI",
+  authDomain: "genius-car-services-1a112.firebaseapp.com",
+  projectId: "genius-car-services-1a112",
+  storageBucket: "genius-car-services-1a112.appspot.com",
+  messagingSenderId: "156703386255",
+  appId: "1:156703386255:web:280af801a9378e4ec85efa"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export default auth;
