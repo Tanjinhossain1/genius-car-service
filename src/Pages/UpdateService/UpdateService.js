e02d9351651e396d0999d316efc92709654667f6
tanjinhossain1 <tanjinhossain2003@gmail.com>
2022-04-22 15:26:57 +0600

    update user backend and client site done

--- src/Pages/UpdateService/UpdateService.js
+++ src/Pages/UpdateService/UpdateService.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 // import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
@@ -10,10 +11,36 @@ const UpdateService = () => {
     //     .then(res => res.json())
     //     .then(data => setService(data))
     // },[id])
+    const {
+        register,
+        handleSubmit,
+   
+    } = useForm();
+    const onSubmit = (data) => {
+        console.log(data);
+        const url = `http://localhost:5000/service/${id}`;
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Content-Type':
+                    'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(res => res.json())
+        .then(result => console.log(result))
+    };
    
     return (
-        <div className='all-margin'>
-            <h1 >update Service: {id} </h1>
+        <div className='all-margin w-50 mx-auto mb-5'>
+            <h4 >update Service: {id} </h4>
+            <form className='d-flex flex-column' onSubmit={handleSubmit(onSubmit)}>
+                <input placeholder='Name' className='mb-2' {...register('name')} />
+                <textarea placeholder='Description' className='mb-2' {...register('description', { required: true })} />             
+                <input placeholder='Price' className='mb-2' {...register('price')} />
+                <input placeholder='Img Url' className='mb-2' {...register('img')} />
+                <input className='btn btn-success' type="submit" value='Update Service' />
+            </form>
         </div>
     );
 };
